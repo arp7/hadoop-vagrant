9bc12274a2efa47363cf6c36324701734cb93e4c
Arpit Agarwal <arp@apache.org>
2017-10-18 12:41:50 -0700

    Use custom base box.
    Reduce provisioning time by using custom base box with some
    pre-installed utilities.

--- centos7/provisioning/bootstrap.sh
+++ centos7/provisioning/bootstrap.sh
@@ -9,14 +9,6 @@
 cp /vagrant/dns-files/hosts /etc/hosts
 cp /vagrant/dns-files/resolv.conf /etc/resolv.conf
 
-# Enable password-less ssh for root between the VMs.
-#
-mkdir -p /root/.ssh
-cp /vagrant/ssh-files/* /root/.ssh/
-chmod 600 /root/.ssh
-chmod 600 /root/.ssh/id_rsa*
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-
 yum install -y ntp
 service ntpd start
 service iptables stop
@@ -35,27 +27,6 @@ echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab
 # Workaround for missing network. https://github.com/mitchellh/vagrant/issues/8096
 #
 sudo service network restart
-
-# Install vim and make it more pleasant to use.
-#
-yum install -y vim
-echo "" >> /etc/vimrc
-echo "set ic is ai et" >> /etc/vimrc
-echo "set ts=2" >> /etc/vimrc
-echo "set sw=2" >> /etc/vimrc
-echo "set background=dark" >> /etc/vimrc
-echo "" >> /root/.bashrc
-echo "alias vi=vim" >> /root/.bashrc
-
-# A few more tools.
-#
-yum install -y epel-release
-yum install -y wget curl net-tools htop
-
-# Install Oracle JDK 8.
-#
-yum -y install openssl openssl-devel
-yum install -y /vagrant/jdk-8u131-linux-x64.rpm
 
 # Create a couple of directories for Apache Hadoop.
 #
