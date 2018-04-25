1e8cba3e2ca4b219b5e6409aa5cc433e987fa0b6
Arpit Agarwal <arp@apache.org>
2018-04-25 13:33:01 -0700

    Allow disabling Smartsense for HDP 3.0

--- centos7/provisioning/install-ambari.sh
+++ centos7/provisioning/install-ambari.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Exit if no Ambari repo URL was provided.
 [[ -z $AMBARI_REPO_URL ]] && exit
 
 # Setup the repo and install Ambari server.
@@ -14,8 +15,16 @@ ambari-server setup -s
 
 # Allow deselecting SmartSense while installing the cluster.
 #
-grep -Fv '<selection>MANDATORY</selection>' /var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE/metainfo.xml > /tmp/metainfo.xml
-cat /tmp/metainfo.xml > /var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE/metainfo.xml
+SMARTSENSE_METAINFO_2_1=/var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE/metainfo.xml
+SMARTSENSE_METAINFO_3_0=/var/lib/ambari-server/resources/stacks/HDP/3.0/services/SMARTSENSE/metainfo.xml
+
+for metainfo_file in $SMARTSENSE_METAINFO_2_1 $SMARTSENSE_METAINFO_3_0
+do
+  if [ -f $metainfo_file ]; then
+    grep -Fv '<selection>MANDATORY</selection>' $metainfo_file > /tmp/metainfo.xml
+    cat /tmp/metainfo.xml > $metainfo_file
+    rm -f /tmp/metainfo.xml
+  fi
+done
 
 ambari-server start
-
