f870836fa478f621c93aa9dcc37be9d5ca4f562f
Arpit Agarwal <arp@apache.org>
2019-06-14 08:15:49 -0700

    Bugfix in OpenJDK installation.

--- centos7/up.sh
+++ centos7/up.sh
@@ -13,8 +13,6 @@ set -euo pipefail
 AMBARI_REPO=${3-}
 [[ -n ${AMBARI_REPO} ]] && echo "Will install Ambari from $AMBARI_REPO"
 
-DEFAULT_JDK_RPMS="java-1.8.0-openjdk-devel-1.8.0.191.b12-0.el7_5.x86_64.rpm"
-RPM_BASE_URL=http://mirror.centos.org/centos/7/updates/x86_64/Packages
 JDK_INSTALLER_FILES=${2-}
 
 # Ensure the vagrant-vbguest plugin is installed.
@@ -24,17 +22,26 @@ if ! vagrant plugin list | grep -qi vagrant-vbguest; then
   vagrant plugin install vagrant-vbguest
 fi
 
-# No JDK package provided on the command-line. Download the default.
+# Look for JDK RPM.
 #
+shopt -s nullglob
+DEFAULT_OPEN_JDK_RPMS=$(echo *openjdk*.rpm)
+DEFAULT_OPEN_JDK_TARBALL=$(echo *openjdk*.tar*)
+
 if [[ -z "${JDK_INSTALLER_FILES}" ]]; then
-    echo "No JDK installer specified, I will use the default"
-    for f in ${DEFAULT_JDK_RPMS}; do
-        [[ ! -f "${f}" ]] && wget "${RPM_BASE_URL}/${f}"
-    done
-    JDK_INSTALLER_FILES="${DEFAULT_JDK_RPMS}"
-else
-    echo "I will install the JDK using ${JDK_INSTALLER_FILES}."
+  echo "No JDK installer specified, I will look for OpenJDK RPM..."
+  JDK_INSTALLER_FILES="${DEFAULT_OPEN_JDK_RPMS}"
+  if [[ -z ${JDK_INSTALLER_FILES} ]]; then
+    echo "No OpenJDK RPM found, looking for tarballs..."
+    JDK_INSTALLER_FILES="${DEFAULT_OPEN_JDK_TARBALL}"
+    if [[ -z ${JDK_INSTALLER_FILES} ]]; then
+      echo "No OpenJDK RPM or tarball found in install directory. Giving up."
+      exit 2
+    fi
+  fi
 fi
+
+echo "I will install the JDK using ${JDK_INSTALLER_FILES}."
 
 # Start the VMs, setting environment variables to locate JDK RPMs and
 # Ambari repo URL.
